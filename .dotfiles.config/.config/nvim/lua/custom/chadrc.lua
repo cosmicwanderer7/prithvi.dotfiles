33605c7e8a53910f5e6b8c3e524756ed2a1e2a62
cosmicwanderer7 <yewaleprithvi2003@gmail.com>
2024-04-03 11:11:07 +0530

    dracula

--- .dotfiles.config/.config/nvim/lua/custom/chadrc.lua
+++ .dotfiles.config/.config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "chadracula",
-	theme_toggle = { "chadracula", "chadracula" },
+	theme = "rosepine",
+	theme_toggle = { "rosepine", "rosepine" },
 
 	hl_override = highlights.override,
 	hl_add = highlights.add,
